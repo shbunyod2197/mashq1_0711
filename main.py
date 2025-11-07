c7eef058a01e3fb71d9ee7ad17545ac303479af2
shbunyod2197 <shbunyod2197@gmail.com>
2025-11-07 17:03:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,77 @@
+# 01-m
+for i in range(1, 51):
+    print(i)
+    if i == 42:
+        break
+# 02-m
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+# 03-m
+for i in range(1, 31):
+    print(i)
+    if i % 5 == 0:
+        break
+# 04-m
+for i in range(1, 16):
+    if i % 3 != 0:
+        print(i)
+# 05-m
+for i in range(1, 21):
+    if i == 25:
+        print(f"25")
+# 06-m
+for i in range(1, 51):
+    if i % 7 == 0:
+        print(i)
+07-m
+for i in range(1, 21):
+    print(i)
+    if i == 10:
+        break
+# 08-m
+for i in range(1, 16):
+    if i % 2 != 0:
+        print(i)
+# 09-m
+for i in range(10, 1, -1):
+    print(i)
+    if i == 5:
+        break
+# 10-m
+for i in range(1, 26):
+    if i % 4 == 0:
+        print(i)
+# 11-m
+for i in range(1, 21):
+    print(i)
+    if i == 15:
+        break
+# 12-m
+for i in range(1, 101):
+    if i % 9 == 0:
+        print(i)
+        break
+# 13-m
+for i in range(1, 21):
+    if i % 2 or i % 3 != 0:
+        print(i)
+# 14-m
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
+# 15-m
+for i in range(1, 7):
+    if i % 8 == 0:
+        break
+else:
+    print("bo'linadigan son yo'q")
+# 16-m
+for i in range(16, 1, -1):
+    if i % 2 != 0:
+        print(i)
+# 17-m
+for i in range(1, 21):
+    print(i)
+    if i == 12:
+        break
